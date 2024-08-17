8cc9a25f0b2620ef56aade624202cb76cbf25c51
sa96125 <sa96125@gmail.com>
2024-08-17 21:22:35 +0900

    feat: Add custom screen sizes and animations to Tailwind config
    - Added custom screen breakpoints for mobile, tablet, and desktop
    - Added 'zoomIn' and 'fadeInDelayed' keyframe animations
    - Configured new animations for smooth transitions and delayed effects

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { transform } from "next/dist/build/swc";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -8,10 +9,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        mobile: "735px",
+        tablet: "834px",
+        desktop: "1024px",
+      },
+
+      keyframes: {
+        zoomIn: {
+          "0%": { transform: "scale(1.2)", opacity: "0" },
+          "100%": { transform: "scale(1)", opacity: "1" },
+        },
+        fadeInDelayed: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        zoomIn: "zoomIn 1.2s ease-out forwards",
+        fadeInDelayed: "fadeInDelayed .6s ease-in-out .6s forwards", // 1.5초 지연 후 1초 동안 애니메이션 실행
       },
     },
   },
