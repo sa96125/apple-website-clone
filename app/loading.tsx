835680f03f8d15198741ffd2a88ad8553d1bed4d
sa96125 <sa96125@gmail.com>
2024-08-20 00:44:24 +0900

    feat: add Loading component with centered skeleton

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import LoadingSkeleton from "@/components/LoadingSkeleton";
+
+export default function Loading() {
+  <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <LoadingSkeleton />
+  </div>;
+}
