7107257b1b64fc68d9c679986a2a2eebc16af048
sa96125 <sa96125@gmail.com>
2024-08-20 01:40:05 +0900

    feat: Add CarouselSection component

---
+++ components/section-carousel/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function CarouselSection() {
+  return (
+    <section className="section-content" id="scroll-section-6">
+      <p>
+        5가지 색상.
+        <br /> 단조로움을 거부한다.
+      </p>
+    </section>
+  );
+}
+
+export default CarouselSection;
